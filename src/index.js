0b8ba65e1e24eb75265ef8847df181a23c032ecd
informagico <alessandro.magoga92@gmail.com>
2021-08-19 00:46:21 +0200

    Added support for Telegram notifications. Added support for environment variable values (with priority over config.js). Added minimal webserver to avoid no-web-access SaaS killing policy.

--- src/index.js
+++ src/index.js
@@ -4,56 +4,105 @@ import cron from "node-schedule";
 import cronstrue from "cronstrue";
 import { SendGridNotification } from "./services/sendgrid-notification.js";
 import colors from "colors";
+import { TelegramAPI } from "./services/telegram-api.js"
+import http from "http";
 
-const notification = new SendGridNotification(config.sendgrid_secret);
+/**
+ * Simple HTTP server (so Heroku and other free SaaS will not bother on killing the app on free plans)
+ * Can always use something like Kaffeine to keep it alive
+ */
+const PORT = process.env.PORT || config.port || 3000;
+const requestListener = function (req, res) {
+	res.writeHead(200);
+	res.end('Hello, World!');
+}
+const server = http.createServer(requestListener);
+server.listen(PORT);
+
+/**
+ * Binance Integration
+ */
+const BINANCE_KEY = process.env.BINANCE_KEY || config.binance_key;
+const BINANCE_SECRET = process.env.BINANCE_SECRET || config.binance_secret;
+const binance = new BinanceAPI(BINANCE_KEY, BINANCE_SECRET);
+
+/**
+ * Telegram Integration
+ */
+const TELEGRAM_TOKEN = process.env.TELEGRAM_TOKEN || config.telegram_token;
+const TELEGRAM_CHAT_ID = process.env.TELEGRAM_CHAT_ID || config.telegram_chat_id;
+const telegram = new TelegramAPI(TELEGRAM_TOKEN, TELEGRAM_CHAT_ID);
+
+/**
+ * SendGrid Integration
+ */
+const SENDGRID_SECRET = process.env.SENDGRID_SECRET || config.sendgrid_secret;
+const SENDGRID_FROM = process.env.SENDGRID_FROM || config.notifications.from;
+const SENDGRID_TO = process.env.SENDGRID_TO || config.notifications.to;
+const sendGrid = new SendGridNotification(SENDGRID_SECRET, SENDGRID_TO, SENDGRID_FROM);
 
 /**
  * @param {object} coin
  */
 async function placeOrder(coin) {
-  const api = new BinanceAPI(config.binance_key, config.binance_secret);
-  const { asset, currency, quantity, quoteOrderQty } = coin;
-  const pair =  asset + currency;
-  const response = await api.marketBuy(pair, quantity, quoteOrderQty);
+	const { asset, currency, quantity, quoteOrderQty } = coin;
+	const pair = asset + currency;
+	const response = await binance.marketBuy(pair, quantity, quoteOrderQty);
 
-  if (response.orderId) {
-    const successText = `Successfully purchased: ${response.executedQty} ${pair} @ ${response.fills[0].price}. Spend: ${response.cummulativeQuoteQty}.\n`;
-    const data = `${JSON.stringify(response)}\n`;
+	if (response.orderId) {
+		const successText = `Successfully purchased: ${response.executedQty} ${asset} @ ${response.fills[0].price} ${currency}. Spent: ${response.cummulativeQuoteQty} ${currency}.\n`;
+		const data = `${JSON.stringify(response)}\n`;
 
-    console.log(colors.green(successText), colors.grey(data));
-    await notification.send(config.notifications.to, config.notifications.from, `Buy order executed (${pair})`, successText + data);
-  } else {
-    const errorText = response.msg || `Unexpected error placing buy order for ${pair}`;
-    console.error(colors.red(errorText));
-    await notification.send(config.notifications.to, config.notifications.from, `Buy order failed (${pair})`, errorText);
-  }
+		console.log(colors.green(successText), colors.grey(data));
+
+		await sendGrid.send(`Buy order executed (${pair})`, successText + data);
+
+		const details = binance.getOrderDetails("BTC", "EUR", response);
+		await telegram.sendMessage(`✅ *Buy order executed (${pair})*\n\n` +
+			`_Order ID:_ ${details.orderId}\n` +
+			`_Date:_ ${details.transactionDateTime}\n` +
+			`_Quantity:_ ${details.quantity} ${details.asset}\n` +
+			`_Total:_ ${details.totalCost} ${details.currency}\n` +
+			`_Average Value:_ ${details.averageAssetValue} ${details.currency}/${details.asset}\n` +
+			`_Fees:_ ${details.commissions} ${details.commissionAsset}\n\n` +
+			`${details.fills.join('\n')}`);
+	} else {
+		const errorText = response.msg || `Unexpected error placing buy order for ${pair}`;
+		console.error(colors.red(errorText));
+
+		await sendGrid.send(`Buy order failed(${pair})`, errorText);
+		await telegram.sendMessage(`❌ * Buy order failed (${pair}) *\n\n` +
+			'```' +
+			`${errorText}` +
+			'```');
+	}
 }
 
 // Loop through all the assets defined to buy in the config and schedule the cron jobs
 async function runBot() {
-  console.log(colors.magenta("Starting Binance DCA Bot"), colors.grey(`[${new Date().toLocaleString()}]`));
+	console.log(colors.magenta("Starting Binance DCA Bot"), colors.grey(`[${new Date().toLocaleString()}]`));
 
-  for (const coin of config.buy) {
-    const { schedule, asset, currency, quantity, quoteOrderQty } = coin;
+	for (const coin of config.buy) {
+		const { schedule, asset, currency, quantity, quoteOrderQty } = coin;
 
-    if (quantity && quoteOrderQty) {
-      throw new Error(`Error: You can not have both quantity and quoteOrderQty options at the same time.`);
-    }
+		if (quantity && quoteOrderQty) {
+			throw new Error(`Error: You can not have both quantity and quoteOrderQty options at the same time.`);
+		}
 
-    if (quantity) {
-      console.log(colors.yellow(`CRON set up to buy ${quantity} ${asset} with ${currency} ${schedule ? cronstrue.toString(schedule) : "immediately."}`));
-    } else {
-      console.log(colors.yellow(`CRON set up to buy ${quoteOrderQty} ${currency} of ${asset} ${schedule ? cronstrue.toString(schedule) : "immediately."}`));
-    }
+		if (quantity) {
+			console.log(colors.yellow(`CRON set up to buy ${quantity} ${asset} with ${currency} ${schedule ? cronstrue.toString(schedule) : "immediately."}`));
+		} else {
+			console.log(colors.yellow(`CRON set up to buy ${quoteOrderQty} ${currency} of ${asset} ${schedule ? cronstrue.toString(schedule) : "immediately."}`));
+		}
 
-    // If a schedule is not defined, the asset will be bought immediately
-    // otherwise a cronjob is setup to place the order on a schedule
-    if (!schedule) {
-      await placeOrder(coin);
-    } else {
-      cron.scheduleJob(schedule, async () => await placeOrder(coin));
-    }
-  }
+		// If a schedule is not defined, the asset will be bought immediately
+		// otherwise a cronjob is setup to place the order on a schedule
+		if (!schedule) {
+			await placeOrder(coin);
+		} else {
+			cron.scheduleJob(schedule, async () => await placeOrder(coin));
+		}
+	}
 }
 
 await runBot();
--- src/services/binance-api.js
+++ src/services/binance-api.js
@@ -3,59 +3,78 @@ import crypto from "crypto";
 import querystring from "querystring";
 
 export class BinanceAPI {
-  /**
-   * @param {string} key
-   * @param {string} secret
-   */
-  constructor(key, secret) {
-    this.apiUrl = "https://api.binance.com";
-    this.key = key;
-    this.secret = secret;
-  }
+	/**
+	 * @param {string} key
+	 * @param {string} secret
+	 */
+	constructor(key, secret) {
+		this.apiUrl = "https://api.binance.com";
+		// this.apiUrl = "https://testnet.binance.vision";
+		this.key = key;
+		this.secret = secret;
+	}
 
-  /**
-   * @param {object} params
-   */
-  createSignature(params) {
-    return crypto
-      .createHmac("sha256", this.secret)
-      .update(querystring.stringify(params))
-      .digest("hex");
-  }
+	/**
+	 * @param {object} params
+	 */
+	createSignature(params) {
+		return crypto
+			.createHmac("sha256", this.secret)
+			.update(querystring.stringify(params))
+			.digest("hex");
+	}
 
-  /**
-   * @param {string} symbol
-   * @param {string} quantity
-   * @param {string} quoteOrderQty
-   */
-  async marketBuy(symbol, quantity, quoteOrderQty) {
-    let params = {
-      symbol: symbol,
-      side: "BUY",
-      type: "MARKET",
-      recvWindow: 30000,
-      timestamp: Date.now()
-    }
+	/**
+	 * @param {string} symbol
+	 * @param {string} quantity
+	 * @param {string} quoteOrderQty
+	 */
+	async marketBuy(symbol, quantity, quoteOrderQty) {
+		let params = {
+			symbol: symbol,
+			side: "BUY",
+			type: "MARKET",
+			recvWindow: 30000,
+			timestamp: Date.now(),
+		}
 
-    if (quantity) params.quantity = quantity;
-    if (quoteOrderQty) params.quoteOrderQty = quoteOrderQty;
+		if (quantity) params.quantity = quantity;
+		if (quoteOrderQty) params.quoteOrderQty = quoteOrderQty;
 
-    params.signature = this.createSignature(params);
+		params.signature = this.createSignature(params);
 
-    const url = `${this.apiUrl}/api/v3/order?${querystring.stringify(params)}`;
+		const url = `${this.apiUrl}/api/v3/order?${querystring.stringify(params)}`;
 
-    try {
-      const response = await fetch(url, {
-        method: "POST",
-        headers: {
-          "X-MBX-APIKEY": this.key,
-          "Content-Type": "application/json"
-        }
-      });
+		try {
+			const response = await fetch(url, {
+				method: "POST",
+				headers: {
+					"X-MBX-APIKEY": this.key,
+					"Content-Type": "application/json"
+				}
+			});
 
-      return await response.json();
-    } catch(error) {
-      console.error(error);
-    }
-  }
+			return await response.json();
+		} catch (error) {
+			console.error(error);
+		}
+	}
+
+	/**
+	 * @param {object} order
+	 */
+	getOrderDetails(asset, currency, order) {
+		return {
+			asset,
+			currency,
+			orderId: order.orderId,
+			transactionDateTime: new Date(order.transactTime).toLocaleString("en-US"),
+			quantity: Math.round(order.executedQty),
+			totalCost: order.fills.reduce((total, f) => (f.price * f.qty) + total, 0),
+			averageAssetValue: order.fills.reduce((total, f) => (f.price * f.qty) + total, 0) / order.executedQty,
+			commissions: order.fills.reduce((total, obj) => (obj.commission * 1.0) + total, 0),
+			commissionAsset: order.fills[0].commissionAsset,
+			fills: order.fills.map(({ price, qty, commission, commissionAsset }) => `💰 ${Math.round(qty)} ${asset} x ${Math.round(price)} ${currency} (fee: ${Math.round(commission)} ${commissionAsset})`)
+		}
+	}
 }
